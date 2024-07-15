80c4c0081c7d7fd8c5527d217810e69c18733616
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2024-07-15 14:34:42 +0200

    Add a “copy debug info” button to context menu

--- src/background.js
+++ src/background.js
@@ -44,6 +44,12 @@ function createMenuItems() {
     title: 'Sponsor this project',
     contexts: ['action']
   })
+
+  chrome.contextMenus.create({
+    id: 'copy_debug_info',
+    title: 'Copy debug info',
+    contexts: ['action']
+  })
 }
 
 /**
@@ -163,9 +169,9 @@ function onAction(tab) {
  *
  * @param {chrome.contextMenus.OnClickData} info
  * @param {chrome.tabs.Tab} tab
- * @returns {void}
+ * @returns {Promise<void>}
  */
-function onMenuItemClicked(info, tab) {
+async function onMenuItemClicked(info, tab) {
   switch (info.menuItemId) {
     case 'open_nano':
       nano.open({
@@ -187,6 +193,23 @@ function onMenuItemClicked(info, tab) {
     case 'open_sponsorship_page':
       openNewTabRight(tab, 'https://github.com/sponsors/taupiqueur')
       break
+
+    case 'copy_debug_info': {
+      const debugInfo = await getDebugInfo()
+
+      await chrome.scripting.executeScript({
+        target: {
+          tabId: tab.id
+        },
+        func: (text) => {
+          return navigator.clipboard.writeText(text)
+        },
+        args: [
+          JSON.stringify(debugInfo, null, 2)
+        ]
+      })
+      break
+    }
   }
 }
 
@@ -213,6 +236,48 @@ async function openNewTabRight(openerTab, url) {
         createdTab.id
       ]
     })
+  }
+}
+
+/**
+ * Returns debug info.
+ *
+ * https://github.com/lydell/LinkHints/blob/main/src/popup/Program.tsx
+ *
+ * @typedef {object} DebugInfo
+ * @property {string} version
+ * @property {string} userAgent
+ * @property {chrome.runtime.PlatformInfo} platformInfo
+ * @property {object} syncStorage
+ * @property {object} localStorage
+ * @property {object} sessionStorage
+ * @property {string} language
+ *
+ * @returns {Promise<DebugInfo>}
+ */
+async function getDebugInfo() {
+  const manifest = chrome.runtime.getManifest()
+
+  const [
+    platformInfo,
+    syncStorage,
+    localStorage,
+    sessionStorage,
+  ] = await Promise.all([
+    chrome.runtime.getPlatformInfo(),
+    chrome.storage.sync.get(),
+    chrome.storage.local.get(),
+    chrome.storage.session.get(),
+  ])
+
+  return {
+    version: manifest.version,
+    userAgent: navigator.userAgent,
+    platformInfo,
+    syncStorage,
+    localStorage,
+    sessionStorage,
+    language: navigator.language,
   }
 }
 
